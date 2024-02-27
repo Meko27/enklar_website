cf969c4fc9ed748ce932f20663b6acb698851073
Meko27 <meihenkohsar@msn.com>
2024-02-27 12:16:18 +0100

    Reduced padding between banner and feature

--- src/layouts/partials/Advantages.tsx
+++ src/layouts/partials/Advantages.tsx
@@ -27,7 +27,7 @@ const Advantages = ({ data }: { data: PageData }) => {
   return (
     <>
       {data.frontmatter.enable && (
-        <section className="section">
+        <section className="section pt-2">
           <div className="container">
             <div className="row">
               <div className="mx-auto mb-12 text-center md:col-10 lg:col-8 xl:col-6">
