cf86ab33bd12b434189c3853d84c1d3e9de585b3
Meko27 <meihenkohsar@msn.com>
2024-03-20 13:48:15 +0100

    CHanged sitemap and removed example.com

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://example.com",
-  generateRobotsTxt: true,
+  siteUrl: process.env.SITE_URL || "https://enklar.de",
+  generateRobotsTxt: true, // Optional: Also generate a robots.txt
 };
