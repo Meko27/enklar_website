b6da8923371d534cac290a4c16cc32908ee2ec9e
Meko27 <meihenkohsar@msn.com>
2024-06-20 09:59:28 +0200

    update robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,3 +3,16 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://enklar.de",
   generateRobotsTxt: true, // Optional: Also generate a robots.txt
 };
+
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://enklar.de",
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', disallow: '/datenschutzerklaerung' },
+      { userAgent: '*', disallow: '/impressum' },
+    ],
+  },
+};
