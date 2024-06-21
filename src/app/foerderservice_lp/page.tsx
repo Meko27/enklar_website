f14bb505493aaf7947e9aa59f1c442e6a1d72aab
Meko27 <meihenkohsar@msn.com>
2024-06-21 17:23:30 +0200

    added new flow

--- src/app/foerderservice_lp/page.tsx
+++ src/app/foerderservice_lp/page.tsx
@@ -144,7 +144,7 @@ const Foerderservice = async () => {
           </div>
 
           <div className="mt-1 flex justify-center">
-            <heyflow-wrapper flow-id="heizungstausch_embed" dynamic-height style-config='{"width": "1000px"}'></heyflow-wrapper>
+            <heyflow-wrapper flow-id="lp-heizungstausch" dynamic-height scroll-up-on-navigation style-config='{"width": "auto"}'></heyflow-wrapper>
           </div>
 
         </div>
